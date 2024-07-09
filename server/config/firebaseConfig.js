db140f5fec0f9c5003ba641856082391d0f5cc5c
vanivineela-aremanda <vanivineelaaremanda@sjsu.edu>
2024-07-09 14:29:56 -0700

    Post_implementation_except_file upload

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,12 +1,13 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json'); // Adjust path as needed
+const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json"); // Adjust path as needed
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  projectId: 'curriculum-portal-1ce8f',
-  storageBucket: 'curriculum-portal-1ce8f.appspot.com'
+  projectId: "curriculum-portal-1ce8f",
+  storageBucket: "curriculum-portal-1ce8f.appspot.com",
 });
 
 const db = admin.firestore();
+const storage = admin.storage();
 
-module.exports = { admin, db };
+module.exports = { admin, db, storage };
