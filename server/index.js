1f0560b126486376fe37f831cf19460728c05ae9
yunqilee <yli380479@gmail.com>
2024-07-10 14:55:58 -0500

    Update index.js
    Update the server side code running port, so it won't have conflicts with the client side code.

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api", unitsRoutes); // Prefix all routes with /api
 app.use("/api", contentRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
