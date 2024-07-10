0c3f1d9e667a801ad161505db10454d3937d77c4
vanivineela-aremanda <vanivineelaaremanda@sjsu.edu>
2024-07-09 19:50:37 -0700

    Post method working fine. checked with postman

--- server/routes/units.js
+++ server/routes/units.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const { getAllUnits, getUnitById } = require('../controllers/unitsController');
+const express = require("express");
+const { getAllUnits, getUnitById } = require("../controllers/unitsController");
+const { post_content } = require("../controllers/content_submission");
 
 const router = express.Router();
+router.use(express.json());
 
-router.get('/units', getAllUnits);
-router.get('/unit/:id', getUnitById);
+router.get("/units", getAllUnits);
+router.get("/unit/:id", getUnitById);
+router.post("/content", post_content);
 
 module.exports = router;
