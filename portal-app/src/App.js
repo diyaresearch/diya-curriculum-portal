60e5353d515ae9052c85eed6697642eab5e7c5fc
yunqilee <yli380479@gmail.com>
2024-07-16 16:47:22 -0500

    Reconstruct client side code

--- portal-app/src/App.js
+++ portal-app/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import "./index.css";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import { UploadContent } from "./pages/upload-content";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/upload-content" element={<UploadContent />} />
       </Routes>
     </BrowserRouter>
