61bf78ca4a3fbddfff088f1d8d91e3754558f30c
yunqilee <yli380479@gmail.com>
2024-07-23 15:09:23 -0500

    Fix error of the rest api

--- server/routes/units.js
+++ server/routes/units.js
@@ -4,15 +4,13 @@ const { post_content } = require("../controllers/content_submission");
 const { updateUnitById } = require("../controllers/update_submission");
 const authenticateUser = require("../middleware/authenticateUser");
 
-
 const router = express.Router();
 router.use(express.json());
 
 router.get("/units", getAllUnits);
 router.get("/unit/:id", getUnitById);
-router.post("/content", post_content);
-router.put("/update/:id", updateUnitById);
+// router.post("/content", post_content);
+router.post("/update/:id", updateUnitById);
 router.post("/content", authenticateUser, post_content); // Apply middleware here
-
 
 module.exports = router;
