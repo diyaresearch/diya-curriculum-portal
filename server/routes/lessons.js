3a2926408c5c9bb3d44e7e04b5971fc7f5c40a52
yunqilee <yli380479@gmail.com>
2024-08-23 19:34:36 -0500

    Implement pdf download api

--- server/routes/lessons.js
+++ server/routes/lessons.js
@@ -5,6 +5,7 @@ const { getLessonById } = require("../controllers/lessonsController");
 const { getAllSections } = require("../controllers/lessonsController");
 const { getSections } = require("../controllers/lessonsController");
 const { postLesson } = require("../controllers/lessonsController");
+const { downloadPDF } = require("../controllers/lessonsController");
 
 const router = express.Router();
 router.use(express.json());
@@ -14,5 +15,6 @@ router.get("/lesson/:lessonId", getLessonById);
 router.get("/sections", getAllSections);
 router.get("/lessons/sections", getSections);
 router.post("/lesson", postLesson);
+router.get("/lessons/:lessonId/download", downloadPDF);
 
 module.exports = router;
