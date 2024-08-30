72fc4561fae5d84abf37115c151de7a10555c2db
Jia Hou <jiahou961@gmail.com>
2024-08-29 18:31:16 -0700

    fixed a few frontend layout

--- portal-app/src/components/TileItem.jsx
+++ portal-app/src/components/TileItem.jsx
@@ -16,12 +16,12 @@ const TileItem = ({
       className="border p-4 rounded-md shadow-sm hover:bg-gray-100 hover:shadow-lg transition duration-200"
       onClick={() => onClick(id)}
     >
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="text-sm text-gray-600">Category: {category}</p>
-      <p className="text-sm text-gray-600">Type: {type}</p>
-      <p className="text-sm text-gray-600">Level: {level}</p>
-      <p className="text-sm text-gray-600">Duration: {duration}</p>
-      <p className="text-sm text-gray-600">Date: {date || "N/A"}</p>
+      <h3 className="text-[1.0625rem] font-semibold text-left mb-2">{title}</h3>
+      <p className="text-sm text-gray-600 text-left">Category: {category}</p>
+      <p className="text-sm text-gray-600 text-left">Type: {type}</p>
+      <p className="text-sm text-gray-600 text-left">Level: {level}</p>
+      <p className="text-sm text-gray-600 text-left">Duration: {duration}min</p>
+      <p className="text-sm text-gray-600 text-left">Date: {date || "N/A"}</p>
     </div>
   );
 };
