9413f677b8b6ae02557c5a0e7673abe0ed862255
Jia Hou <jiahou961@gmail.com>
2024-08-30 14:55:41 -0700

    improved the layout

--- portal-app/src/components/TileItem.jsx
+++ portal-app/src/components/TileItem.jsx
@@ -13,15 +13,15 @@ const TileItem = ({
 }) => {
   return (
     <div
-      className="border p-4 rounded-md shadow-sm hover:bg-gray-100 hover:shadow-lg transition duration-200"
+      className="relative border p-4 rounded-md shadow-sm hover:bg-gray-100 hover:shadow-lg transition duration-200 h-60 flex flex-col justify-between"
       onClick={() => onClick(id)}
     >
-      <h3 className="text-[1.0625rem] font-semibold text-left mb-2">{title}</h3>
-      <p className="text-sm text-gray-600 text-left">Category: {category}</p>
-      <p className="text-sm text-gray-600 text-left">Type: {type}</p>
-      <p className="text-sm text-gray-600 text-left">Level: {level}</p>
-      <p className="text-sm text-gray-600 text-left">Duration: {duration}min</p>
-      <p className="text-sm text-gray-600 text-left">Date: {date || "N/A"}</p>
+      <h3 className="text-[1.0625rem] font-semibold mb-2">{title}</h3>
+      <p className="text-sm text-gray-600 ">Category: {category}</p>
+      <p className="text-sm text-gray-600 ">Type: {type}</p>
+      <p className="text-sm text-gray-600 ">Level: {level}</p>
+      <p className="text-sm text-gray-600 ">Duration: {duration}min</p>
+      <p className="text-sm text-gray-600">Date: {date || "N/A"}</p>
     </div>
   );
 };
