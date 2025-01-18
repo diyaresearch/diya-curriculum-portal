42a1ac3dd3ca33e13740d283115a783212646005
zexigong <zexi.gong721@gmail.com>
2025-01-17 18:28:50 -0800

    update style of navbar

--- portal-app/src/components/Navbar.jsx
+++ portal-app/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {
 } from "firebase/auth";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { auth, db } from "../firebase/firebaseConfig";
+import logo from "../assets/DIYA_Logo.png";
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
@@ -98,18 +99,224 @@ const Navbar = () => {
 
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      <div className="text-white text-xl">DIYA Curriculum Portal</div>
+      <div className="flex items-center space-x-2">
+        <img
+          src={logo}
+          alt="Logo"
+          style={{ height: "113px", width: "90px" }}
+        />
+      </div>
+
       <div className="space-x-4 text-white">
-        <a href="#about" className="hover:underline">
-          About Us
+        <a
+          href="https://diya-research.org/team/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+          style={{
+            fontSize: "18px",
+            fontWeight: "400",
+            fontFamily: "Open Sans, sans-serif",
+            letterSpacing: "1.5px",
+            textUnderlineOffset: "20px", 
+            padding: "0px 20px",
+          }}
+        >
+          Team
         </a>
-        <a href="#resources" className="hover:underline">
-          Resources
-        </a>
-        <a href="#contact" className="hover:underline">
+        <div className="relative group inline-block">
+          <a
+            href="#"
+            className="hover:underline group-hover:underline"
+            style={{
+              fontSize: "18px",
+              fontWeight: "400",
+              fontFamily: "Open Sans, sans-serif",
+              letterSpacing: "1.5px",
+              textUnderlineOffset: "20px",
+              padding: "0px 20px",
+            }}
+          >
+            For Educators
+          </a>
+          {/* Dropdown menu */}
+          <div
+            className="absolute left-1/2 transform -translate-x-1/2 mt-0 hidden group-hover:block hover:block"
+            style={{
+              padding: "20px 0px",
+              margin: "auto",
+              zIndex: 20,
+              overflow: "hidden",
+              minWidth: "180px"
+            }}
+          >
+            <a
+              href="https://diya-research.org/2025-teacher-nomination-form/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              Nominations
+            </a>
+            <a
+              href="https://diya-research.org/professional-development-workshop/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              PD Workshop
+            </a>
+            <a
+              href="https://diya-research.org/ai-ambassador-program/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              AI Ambassador
+            </a>
+          </div>
+        </div>
+        <div className="relative group inline-block">
+          <a
+            href="#"
+            className="hover:underline group-hover:underline"
+            style={{
+              fontSize: "18px",
+              fontWeight: "400",
+              fontFamily: "Open Sans, sans-serif",
+              letterSpacing: "1.5px",
+              textUnderlineOffset: "20px",
+              padding: "0px 20px",
+            }}
+          >
+            For Students
+          </a>
+          <div
+            className="absolute left-1/2 transform -translate-x-1/2 mt-0 hidden group-hover:block hover:block"
+            style={{
+              padding: "20px 0px",
+              margin: "auto",
+              zIndex: 20,
+              overflow: "hidden",
+              minWidth: "180px"
+            }}
+          >
+            <a
+              href="https://diya-research.org/ai-exploration/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              AI Exploration
+            </a>
+            <a
+              href="https://diya-research.org/python-for-ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              Python for AI
+            </a>
+            <a
+              href="https://diya-research.org/ai-insights/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              AI Insights
+            </a>
+            <a
+              href="https://diya-research.org/ai-forge/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              AI Forge
+            </a>
+            <a
+              href="https://diya-research.org/ai-research/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              AI Research
+            </a>
+            <a
+              href="https://diya-research.org/student-showcases/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              Showcases
+            </a>
+            <a
+              href="https://diya-research.org/diya-club-for-high-school/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block px-4 py-2 text-white bg-[#1C418A] hover:bg-gray-500"
+              style={{
+                fontSize: "18px",
+                fontFamily: "Open Sans, sans-serif",
+              }}
+            >
+              DIYA Club
+            </a>
+          </div>
+        </div>
+        <a
+          href="https://diya-research.org/contact-us/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+          style={{
+            fontSize: "18px",
+            fontWeight: "400",
+            fontFamily: "Open Sans, sans-serif",
+            letterSpacing: "1.5px",
+            textUnderlineOffset: "20px", 
+            padding: "0px 20px",
+          }}
+        >
           Contact Us
         </a>
       </div>
+
       <div>
         {!user ? (
           <>
