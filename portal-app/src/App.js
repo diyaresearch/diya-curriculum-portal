3aa81fd559b18f7bc2b71268b55faded6d6d3cbb
zexigong <zexi.gong721@gmail.com>
2025-01-22 15:52:24 -0800

    general edit function added
    Need to change UI; some bugs remains

--- portal-app/src/App.js
+++ portal-app/src/App.js
@@ -8,6 +8,7 @@ import Home from "./pages/home";
 import { UploadContent } from "./pages/upload-content";
 import ViewContent from "./pages/view-content";
 import LessonGenerator from "./pages/lesson_generator";
+import EditLesson from "./pages/edit_lesson";
 import MyPlans from "./pages/my_plan";
 import LessonDetail from "./pages/lesson_detail";
 import Layout from "./components/Layout";
@@ -22,6 +23,7 @@ function App() {
           <Route path="/edit-content/:id" element={<EditContent />} />
           <Route path="/view-content/:UnitID" element={<ViewContent />} />
           <Route path="/lesson-generator" element={<LessonGenerator />} />
+          <Route path="/edit-lesson/:lessonId" element={<EditLesson />} />
           <Route path="/my-plans" element={<MyPlans />} />
           <Route path="/lesson/:lessonId" element={<LessonDetail />} />
         </Routes>
