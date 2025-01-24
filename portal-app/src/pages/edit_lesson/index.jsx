3285be0b02060a9afbb1c4a140633fe3023f160d
zexigong <zexi.gong721@gmail.com>
2025-01-23 17:21:06 -0800

    update UI align with create lesson plan

--- portal-app/src/pages/edit_lesson/index.jsx
+++ portal-app/src/pages/edit_lesson/index.jsx
@@ -449,7 +449,7 @@ export const EditLesson = () => {
                   onClick={handleSubmit}
                   className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700"
                 >
-                  Update Plan
+                  Save
                 </button>
               </div>
             </div>
