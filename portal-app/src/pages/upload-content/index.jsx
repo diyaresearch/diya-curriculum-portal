f3b84123d7747ec7cb4352707843278f50a3a456
VarshaVenky <varshavenkatesh3798@gmail.com>
2025-01-31 21:48:11 -0500

    Fix #55: Redirect to homepage on Close button click

--- portal-app/src/pages/upload-content/index.jsx
+++ portal-app/src/pages/upload-content/index.jsx
@@ -284,7 +284,9 @@ export const UploadContent = () => {
         >
           <h2>{modalMessage}</h2>
           <button
-            onClick={closeModal}
+            onClick={() => {
+              window.location.href = "https://curriculum-portal-1ce8f.web.app/";
+            }}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Close
