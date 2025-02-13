d4ef0e58d750614125a5786b5d31f09fb4b6b2a6
zexigong <108034351+zexigong@users.noreply.github.com>
2025-02-13 00:53:01 -0800

    Update HomePage.jsx (#182)

--- portal-app/src/components/HomePage.jsx
+++ portal-app/src/components/HomePage.jsx
@@ -3,9 +3,11 @@ import content_creator from "../assets/content_creator.png";
 import educator from "../assets/educator.png";
 import lesson_plans from "../assets/lesson_plans.png";
 import { useNavigate } from "react-router-dom";
+import useUserData from "../hooks/useUserData";
 
 const HomePage = ({ user }) => {
   const navigate = useNavigate();
+  const { userData } = useUserData();
   return (
     <div className="text-center mt-1">
       <div className="bg-gray-200 p-1 rounded-lg shadow-lg ">
@@ -29,25 +31,23 @@ const HomePage = ({ user }) => {
               )}
             </p>
           </div>
-          <div>
-            <img
-              src={content_creator}
-              alt="Content Creator Icon"
-              className="mx-auto max-w-xs w-40 h-40"
-            />
-            <p className="mt-4">
-              {user ? (
+          {user && userData?.role === "admin" && (
+            <div>
+              <img
+                src={content_creator}
+                alt="Content Creator Icon"
+                className="mx-auto max-w-xs w-40 h-40"
+              />
+              <p className="mt-4">
                 <button
                   onClick={() => navigate("/upload-content")}
                   className="font-bold py-2 px-4 rounded"
                 >
                   Click here to upload educational contents
                 </button>
-              ) : (
-                "Upload educational contents"
-              )}
-            </p>
-          </div>
+              </p>
+            </div>
+          )}
           <div>
             <img
               src={lesson_plans}
