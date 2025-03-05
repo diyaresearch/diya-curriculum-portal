6b77a354f68d0f4b5159c73618d96ad56ddeda23
VarshaVenky <varshavenkatesh3798@gmail.com>
2025-03-05 11:28:42 -0500

    Update Abstract to Rich Text in Upload and Edit Content

--- portal-app/src/pages/lesson_generator/index.jsx
+++ portal-app/src/pages/lesson_generator/index.jsx
@@ -281,7 +281,7 @@ export const LessonGenerator = () => {
             <select
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="category"
-              value={formData.subject}
+              value={formData.category}
               onChange={handleChange}
               required
             >
