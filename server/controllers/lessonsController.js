b26fe51d50fb552e7f843d348660a8ae9eb39daf
zexigong <zexi.gong721@gmail.com>
2025-04-01 17:15:42 -0700

    remove unuse code

--- server/controllers/lessonsController.js
+++ server/controllers/lessonsController.js
@@ -79,54 +79,6 @@ const getUserLessons = async (req, res) => {
   }
 };
 
-const getAllSections = async (req, res) => {
-  try {
-    const sectionsRef = db
-      .collection(TABLE_LESSON)
-      .doc("XnZzLBMIeKE5dsRXZ5nJ")
-      .collection(TABLE_SECTIONS);
-    const snapshot = await sectionsRef.get();
-
-    if (snapshot.empty) {
-      return res.status(404).json({ message: "No matching documents." });
-    }
-
-    const sections = [];
-    snapshot.forEach((doc) => {
-      sections.push({ id: doc.id, data: doc.data() });
-    });
-
-    res.status(200).json(sections);
-  } catch (error) {
-    console.error("Error getting documents:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-const getSections = async (req, res) => {
-  try {
-    const lessonRef = db.collection(TABLE_LESSON).doc("oeVhJ5KBtbrfk5z3XUdT");
-    const doc = await lessonRef.get();
-
-    if (!doc.exists) {
-      return res.status(404).json({ message: "Lesson not found." });
-    }
-
-    const { sections } = doc.data();
-
-    if (!sections || sections.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No sections found for this lesson." });
-    }
-
-    res.status(200).json(sections);
-  } catch (error) {
-    console.error("Error getting sections:", error);
-    res.status(500).json({ error: "Failed to retrieve sections" });
-  }
-};
-
 const postLesson = async (req, res) => {
   try {
     const formData = req.body;
@@ -328,8 +280,6 @@ module.exports = {
   getAllLessons,
   getLessonById,
   getUserLessons,
-  getAllSections,
-  getSections,
   postLesson,
   updateLesson,
   deleteLessonById,
